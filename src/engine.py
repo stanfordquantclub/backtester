0a4196681ff586f471bc468986bd45eaf71735b7
lukepark2004 <lukepark2004@gmail.com>
2023-02-26 13:55:42 -0800

    added logs class, order class, and total return + sharpe ratio calculation

--- src/engine.py
+++ src/engine.py
@@ -38,7 +38,7 @@ class BacktestTime:
     def reset_seconds_elapsed(self):
         self.seconds_elapsed = 0
 
-class Engine: 
+class Engine:
     def initialize_defaults(self, security_name: str=None, start_cash: float=None, start_date:date=None, end_date:date=None, path_dates=None, filter_paths=None, timezone="US/Eastern", root_path="/srv/sqc/data/us-options-tanq"):
         """
         Initialize the defaults for the engine
