7379ad45bd31537612dac95fdd43fb0eeec1f0ea
Arjun Vikram <arjvik@gmail.com>
2023-03-10 20:16:59 -0800

    Replace create_candles.py with create_candles_fast.py

--- src/create_candles.py
+++ src/create_candles.py
@@ -3,53 +3,7 @@ from tqdm import tqdm
 import os
 import numpy as np
 from datetime import date, time, datetime, timedelta
-import pandas_market_calendars as mcal
-import glob
-import pytz
-import multiprocessing
-import numpy as np
 
-def create_candles_day(asset: str, day_path: str, output_path: str, timezone:str="US/Eastern", processes=1):
-    """
-    Creates the candles for every contract within a day
-    
-    Args:
-        asset (str): name of the asset
-        day_path (str): path to the day
-        output_path (str): path to the output
-        timezone (str): timezone to use for the candles
-    """
-    
-    def create_candles_day_paths(contract_paths, output_path, open_time, close_time):
-        for contract_path in contract_paths:
-            print(contract_path)
-            create_candles(
-                contract_path, 
-                output_path=output_path,
-                start_time=open_time, 
-                end_time=close_time
-            )
-
-    nyse = mcal.get_calendar('NYSE')
-    
-    index = day_path.split("/").index("us-options-tanq")
-    day = day_path.split("/")[index+2]
-    schedule = nyse.schedule(day, day)
-    
-    schedule["market_open"] = schedule["market_open"].dt.tz_convert(pytz.timezone(timezone))
-    schedule["market_close"] = schedule["market_close"].dt.tz_convert(pytz.timezone(timezone))
-    
-    open_time = schedule["market_open"].iloc[0].time()
-    close_time = schedule["market_close"].iloc[0].time()
-    
-    contract_paths = glob.glob(f"{day_path}/{asset}*.csv")
-    contract_paths = np.array_split(contract_paths, processes)
-    
-    for process_index in range(processes):
-        process = multiprocessing.Process(target=create_candles_day_paths, args=(contract_paths[process_index], output_path, open_time, close_time))
- 
-        process.start()
-        
 def create_candles(file_path, output_path, start_time=time(9, 30, 0), end_time=time(16, 0, 0)):
     """
     Description:
@@ -66,95 +20,39 @@ def create_candles(file_path, output_path, start_time=time(9, 30, 0), end_time=t
     """    
     
     df = pd.read_csv(file_path)
-    
-    candle_has_data = False
-    candles = []
-    start_index = 0
-    
+
     # Convert to milliseconds
-    start_time_increment = (datetime.combine(date.today(), start_time) + timedelta(seconds=1)).time() # one second ahead
     start_time = int(start_time.strftime("%H%M%S")) * 1000
-    current_time = int(start_time_increment.strftime("%H%M%S")) * 1000
-    
+
     # Add 1 second to end time to include the last second
     end_time = (datetime.combine(date.today(), end_time) + timedelta(seconds=1)).time()
     end_time = int(end_time.strftime("%H%M%S")) * 1000
-            
-    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        if row["Timestamp"] >= start_time and row["Timestamp"] <= end_time:
-            if row["Timestamp"] < current_time:
-                if not candle_has_data:
-                    start_index = index
-                    candle_has_data = True
-            else:
-                if candle_has_data:
-                    candle_data = df.loc[start_index:index-1]
-                    candle = [current_time]
 
-                    # Sum up the trade volume
-                    candle.append(candle_data[candle_data["Action"] == "T"]["Quantity"].sum())
-                    
-                    # Select only bid data and get min
-                    bid_min = candle_data[candle_data["Side"] == "B"]["Price"].min()
-                    # Get the volume at the min bid price
-                    quantity_bid_min = candle_data[(candle_data["Side"] == "B") & (candle_data["Price"] == bid_min)]["Quantity"].min()
-                    bid_max = candle_data[candle_data["Side"] == "B"]["Price"].max()
-                    quantity_bid_max = candle_data[(candle_data["Side"] == "B") & (candle_data["Price"] == bid_max)]["Quantity"].max()
-                    
-                    ask_min = candle_data[candle_data["Side"] == "A"]["Price"].min()
-                    quantity_ask_min = candle_data[(candle_data["Side"] == "A") & (candle_data["Price"] == ask_min)]["Quantity"].min()
-                    ask_max = candle_data[candle_data["Side"] == "A"]["Price"].max()
-                    quantity_ask_max = candle_data[(candle_data["Side"] == "A") & (candle_data["Price"] == ask_max)]["Quantity"].max()
-                    
-                    metrics = [quantity_bid_min, quantity_bid_max, quantity_ask_min, quantity_ask_max, bid_min, bid_max, ask_min, ask_max]
-                    
-                    for metric in metrics:
-                        if np.isnan(metric):
-                            if len(candles) == 0: 
-                                # If there are no candles yet, set the metric to 0
-                                metric = 0
-                            else:
-                                # If there are candles, set the metric to the last candle's metric
-                                metric = candles[-1][len(candle)]
-                            
-                        candle.append(metric)
-                    
-                    candles.append(candle)
-                    candle_has_data = False
-                else:
-                    if len(candles):
-                        candles.append(candles[-1].copy())
-                        candles[-1][2] = current_time
-                
-                # Increment to the next minute
-                if (current_time/1000) % 100 == 59:
-                    current_time += 41000
-                    
-                    # Increment to the next hour
-                    if (current_time/100000) % 100 == 60:
-                        current_time += 4000000
-                else:
-                    current_time += 1000
-                    
-                if row["Timestamp"] < current_time:
-                    if not candle_has_data:
-                        start_index = index
-                        candle_has_data = True
-                        
-    if current_time != end_time:
-        while current_time < end_time:
-            candles.append(candles[-1].copy())
-            candles[-1][2] = current_time
+    # FILTER OUT ITEMS FROM PRE-MARKET AND POST-MARKET
+    df["TimestampSec"] = df["Timestamp"] // 1000
+    df = df[(df["Timestamp"] >= start_time) & (df["Timestamp"] < end_time)]
 
-            # Increment to the next minute
-            if (current_time/1000) % 100 == 59:
-                current_time += 41000
-                
-                # Increment to the next hour
-                if (current_time/100000) % 100 == 60:
-                    current_time += 4000000
-            else:
-                current_time += 1000
-                
-    df = pd.DataFrame(candles, columns=['Timestamp', 'VolumeTrade', 'QuantityBidMin', 'QuantityBidMax', 'QuantityAskMin', 'QuantityAskMax', 'BidMin', 'BidMax', 'AskMin', 'AskMax'])
-    df.to_csv(os.path.join(output_path, "Candles." + os.path.basename(file_path)), encoding='utf-8', index=False)
+
+    CLOCK_HOURS = np.arange(93000,160001)
+
+    df_min_price =  (df.loc[df.groupby([df['Side'], df['TimestampSec']])['Price'].idxmin()]
+                            [['TimestampSec','Side','Price','Quantity']]
+                        .set_index(['Side', 'TimestampSec'])
+                        .reindex(pd.MultiIndex.from_product([['A','B'], CLOCK_HOURS]))
+                        .fillna(method='ffill')
+                        .unstack(level=0)
+                        .pipe(lambda d: (setattr(d, 'columns', [' '.join(col).strip() for col in d.columns.values]), d)[1])
+                        .rename(columns={"Price A": "PriceAskMin", "Price B": "PriceBidMin", "Quantity A": "QuantityAskMin", "Quantity B": "QuantityBidMin"}))
+    
+    df_max_price =  (df.loc[df.groupby([df['Side'], df['TimestampSec']])['Price'].idxmax()]
+                            [['TimestampSec','Side','Price','Quantity']]
+                        .set_index(['Side', 'TimestampSec'])
+                        .reindex(pd.MultiIndex.from_product([['A','B'], CLOCK_HOURS]))
+                        .fillna(method='ffill')
+                        .unstack(level=0)
+                        .pipe(lambda d: (setattr(d, 'columns', [' '.join(col).strip() for col in d.columns.values]), d)[1])
+                        .rename(columns={"Price A": "PriceAskMax", "Price B": "PriceBidMax", "Quantity A": "QuantityAskMax", "Quantity B": "QuantityBidMax"}))
+    
+    df_all = pd.merge(df_min_price,df_max_price, left_index=True, right_index=True)
+
+    df_all.to_csv(os.path.join(output_path, "Candles." + os.path.basename(file_path)), encoding='utf-8', index_label='TimestampSec')
